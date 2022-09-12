e6cec4ae0cf97a4f09e4f97bfce96e3c8a2bff72
Yehuda Lehrfield <yll2113@columbia.edu>
2022-09-11 22:07:59 -0400

    added minimize maximize functionality for editor and preview

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import {marked} from "marked";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-<i class="fa-solid fa-grip-lines-vertical"></i>
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       input:'',
-      verticalOrientation:false
+      verticalOrientation:false,
+      editorMax:false,
+      previewMax:false
     };
     this.handleEditorChange = this.handleEditorChange.bind(this);
     this.handleOrientationChange = this.handleOrientationChange.bind(this);
+    this.handleEditorMax = this.handleEditorMax.bind(this);
+    this.handlePreviewMax = this.handlePreviewMax.bind(this);
   }
 
   handleEditorChange(e) {
@@ -28,21 +31,38 @@ class App extends Component {
       verticalOrientation: !this.state.verticalOrientation
     });
   }
-
+  handleEditorMax() {
+    this.setState({
+      editorMax: !this.state.editorMax
+    });
+  }
+  handlePreviewMax() {
+    this.setState({
+      previewMax: !this.state.previewMax
+    });
+  }
   render() {
+    let editSizeClass = (this.state.editorMax === true) ? ' maximize' : (this.state.previewMax === true) ? " minimize" : "";
+    let prevSizeClass = (this.state.previewMax === true) ? ' maximize' : (this.state.editorMax === true) ? " minimize" : "";
+    let editMinMaxIcon = (this.state.editorMax === true) ? ' fa-solid fa-compress' : ' fa-solid fa-expand';
+    let prevMinMaxIcon = (this.state.previewMax === true) ? ' fa-solid fa-compress' : ' fa-solid fa-expand';
+    
     let classNames = this.state.verticalOrientation
-      ? ['appContainer-vertical','fa-solid fa-grip-lines','editor-container-vertical','preview-container-vertical','dev-info-vertical']
-      : ['appContainer-horizontal','fa-solid fa-grip-lines-vertical','editor-container-horizontal','preview-container-horizontal','dev-info-horizontal'];
-    // console.log(this.state.input);
-    console.log(this.state.verticalOrientation)
+      ? ['appContainer-vertical','fa-solid fa-grip-lines','editor-container-vertical'+editSizeClass,'preview-container-vertical'+prevSizeClass,editMinMaxIcon,prevMinMaxIcon]
+      : ['appContainer-horizontal','fa-solid fa-grip-lines-vertical','editor-container-horizontal'+editSizeClass,'preview-container-horizontal'+prevSizeClass,editMinMaxIcon,prevMinMaxIcon];
+    console.log('input:' + this.state.input);
+    console.log('vertical:' + this.state.verticalOrientation)
+    console.log('editMax:' + this.state.editorMax)
     return (
-      <div id="app-container" className={classNames[0]}>
-        <div id = "options">
-          <button className = "layoutButton" onClick = {this.handleOrientationChange}><i className={classNames[1]}></i></button>
+      <div id="app-wrapper">
+        <div id="app-container" className={classNames[0]}>
+          <div id = "options">
+            <button className = "layoutButton" onClick = {this.handleOrientationChange}><i className={classNames[1]}></i></button>
+          </div>
+          <Editor input={this.state.input} onChange={this.handleEditorChange} orientClass={classNames[2]} editMinMaxIcon = {classNames[4]} editMinMax = {this.handleEditorMax}/>
+          <Preview text={this.state.input} orientClass={classNames[3]} prevMinMaxIcon = {classNames[5]} prevMinMax = {this.handlePreviewMax}/>
         </div>
-        <Editor input={this.state.input} onChange={this.handleEditorChange} orientClass={classNames[2]} />
-        <Preview text={this.state.input} orientClass={classNames[3]}/>
-        <DevInfo orientation={classNames[4]}/>
+        <DevInfo />
       </div>
     );
   }
@@ -53,6 +73,9 @@ const Editor = (props) => {
     <div id='editor-container' className={props.orientClass}>
       <div id="editor-header" className="header">
         <h3>Editor</h3>
+        <button id="editor-min-max-button" className='action-icon-button' onClick={props.editMinMax}>
+          <i id='min-max-icon' className={"header-icon" + props.editMinMaxIcon}></i>
+        </button>
       </div>
       <div id='editor-text-container'>
         <textarea
@@ -72,6 +95,9 @@ const Preview = (props) => {
     <div id="preview-container" className={props.orientClass}>
       <div id="preview-header" className="header">
         <h3>Preview</h3>
+        <button id="preview-min-max-button" className='action-icon-button' onClick={props.prevMinMax}>
+          <i id='min-max-icon' className={"header-icon" + props.prevMinMaxIcon}></i>
+        </button>
       </div>
       <div id='preview-text-container'>
         <div
@@ -85,7 +111,7 @@ const Preview = (props) => {
 
 const DevInfo = (props) => {
   return (
-    <div id="dev-info" className={props.orientation}>
+    <div id="dev-info">
       <div id="line" />
       <p>Yehuda Lehrfield for FreeCodeCamp</p>
       <div id="icons">
